ae59719818aa9c05d6c17d5e84b312dde7ab1f1a
Vikrant Tyagi <33453392+continuum-vikrant-tyagi@users.noreply.github.com>
2019-07-30 17:47:47 +0530

    Update grafana_template.py

--- grafana_template.py
+++ grafana_template.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
-
+import os
 from jinja2 import Environment, FileSystemLoader
-title = raw_input("Enter title: ")
-namespace = raw_input("Enter namespace: ")
+title = os.environ['title']
+namespace = os.environ['namespace']
+print(title)
+print(namespace)
+#title = raw_input("Enter title: ")
+#namespace = raw_input("Enter namespace: ")
 
-file_loader = FileSystemLoader('template')
-env = Environment(loader=file_loader)
+#file_loader = FileSystemLoader('template')
+#env = Environment(loader=file_loader)
 
-template = env.get_template('sample.txt.j2')
+#template = env.get_template('sample.txt.j2')
 
 
-output = template.render(title=title, namespace=namespace)
-with open(title, 'w') as f:
-	f.write(output)
+#output = template.render(title=title, namespace=namespace)
+#with open(title, 'w') as f:
+	#f.write(output)
 #print(output)
