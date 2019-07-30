4f7ae2c8f1e5c46103a8b1b3ce3da2b27aeb0cf3
Vikrant Tyagi <vikrant.tyagi@continuum.net>
2019-07-31 01:23:33 +0530

    modified files

--- grafana_template.py
+++ grafana_template.py
@@ -3,6 +3,7 @@ import os
 from jinja2 import Environment, FileSystemLoader
 title = os.environ['title']
 namespace = os.environ['namespace']
+alert = os.environ['alert']
 print(title)
 print(namespace)
 #title = raw_input("Enter title: ")
@@ -14,7 +15,7 @@ env = Environment(loader=file_loader)
 template = env.get_template('sample.txt.j2')
 
 
-output = template.render(title=title, namespace=namespace)
+output = template.render(title=title, namespace=namespace, alert=alert)
 with open(title, 'w') as f:
 	f.write(output)
 #print(output)
