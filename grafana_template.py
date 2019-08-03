a73ae097f5cec514ec71beef73bf50a4543a7260
Vikrant Tyagi <vikrant.tyagi@continuum.net>
2019-08-03 13:53:53 +0530

    modified file

--- grafana_template.py
+++ grafana_template.py
@@ -4,6 +4,7 @@ from jinja2 import Environment, FileSystemLoader
 title = os.environ['title']
 namespace = os.environ['namespace']
 consumergroup = os.environ['consumergroup']
+topic = os.environ['topic']
 alert = os.environ['alert']
 
 print(title)
@@ -17,7 +18,7 @@ env = Environment(loader=file_loader)
 template = env.get_template('main.txt.j2')
 
 
-output = template.render(title=title, namespace=namespace, alert=alert, consumergroup=consumergroup)
+output = template.render(title=title, namespace=namespace, alert=alert, consumergroup=consumergroup, topic=topic)
 with open(title, 'w') as f:
 	f.write(output)
 #print(output)
