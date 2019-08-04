a9d2af60d4bb780bc2c8bd380ef72cbc8d8da571
Vikrant Tyagi <vikrant.tyagi@continuum.net>
2019-08-04 19:42:21 +0530

    modified jenkinfile

--- grafana_template.py
+++ grafana_template.py
@@ -9,26 +9,26 @@ memoryalert = os.environ['memoryalert']
 cpualert = os.environ['cpualert']
 cg_lagalert = os.environ['cg_lagalert']
 
-# print(title)
-# print(namespace)
-# print(consumergroup)
-# print(topics)
+print(title)
+print(namespace)
+print(consumergroup)
+print(topics)
 
 # topic = topics.split(",")
-for i in topics:
-	print(i)
+# for i in topics:
+# 	print(i)
 
 
-# file_loader = FileSystemLoader('template')
-# env = Environment(loader=file_loader)
-# env.trim_blocks = True
-# env.lstrip_blocks = True
-# env.rstrip_blocks = True
+file_loader = FileSystemLoader('template')
+env = Environment(loader=file_loader)
+env.trim_blocks = True
+env.lstrip_blocks = True
+env.rstrip_blocks = True
 
-# template = env.get_template('main.txt.j2')
+template = env.get_template('main.txt.j2')
 
 
-# output = template.render(title=title, namespace=namespace, consumergroup=consumergroup, topics=topics, memoryalert=memoryalert, cpualert=cpualert, cg_lagalert=cg_lagalert)
+output = template.render(title=title, namespace=namespace, consumergroup=consumergroup, topics=topics, memoryalert=memoryalert, cpualert=cpualert, cg_lagalert=cg_lagalert)
 # # with open(title, 'w') as f:
 # # 	f.write(output)
-# print(output)
+print(output)
